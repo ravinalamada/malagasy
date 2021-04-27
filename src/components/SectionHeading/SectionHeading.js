63f88c1cd153c8a931551f33ed98c7cba7bfb191
prisca <prisca.ros@onja.org>
2021-04-27 13:00:04 +0300

    I styled the component and updated the storie file

--- src/components/SectionHeading/SectionHeading.js
+++ src/components/SectionHeading/SectionHeading.js
@@ -1,6 +1,20 @@
 import React from 'react';
-import {StyleSheet, SectionList, Text} from 'react-native';
+import {StyleSheet, View, Text} from 'react-native';
 
-export default function SectionHeading({onPress}) {
-  return <Text>Text</Text>;
+const style = StyleSheet.create({
+  text: {
+    fontFamily: 'Inter',
+    fontSize: 18,
+    lineHeight: 22,
+    fontWeight: '600',
+    color: '#111827',
+  },
+});
+
+export default function SectionHeading({title}) {
+  return (
+    <View>
+      <Text style={style.text}>{title}</Text>
+    </View>
+  );
 }
--- src/components/SectionHeading/SectionHeading.stories.js
+++ src/components/SectionHeading/SectionHeading.stories.js
@@ -1,31 +1,45 @@
 import React, {useState} from 'react';
-import {
-  StyleSheet,
-  View,
-  SectionList,
-  SafeAreaView,
-  KeyboardAvoidingView,
-} from 'react-native';
+import {StyleSheet, View, SectionList} from 'react-native';
 import {storiesOf} from '@storybook/react-native';
-import SectionHeading from './SectionHeading';
+import SectionHeading, {ListItem} from './SectionHeading';
 
 const style = StyleSheet.create({
   container: {
-    margin: 20,
+    marginTop: 29,
+    marginLeft: 29,
+    marginBottom: 17,
   },
 });
 
+function Heading() {
+  const [category, setCategory] = useState([{name: 'food', id: '1'}]);
+  return (
+    <SectionList
+      sections={[{title: 'Select a category:', data: category}]}
+      renderSectionHeader={({section}) => (
+        <SectionHeading title={section.title} />
+      )}
+      renderItem={item => null}
+      KeyExtractor={item => item.id}
+    />
+  );
+}
+
+function SecondHeading() {
+  const [learntData, setLearntData] = useState([{name: 'number', id: '2'}]);
+  return (
+    <SectionList
+      sections={[{title: 'Learnt', data: learntData}]}
+      renderSectionHeader={({section}) => (
+        <SectionHeading title={section.title} />
+      )}
+      renderItem={() => null}
+      KeyExtractor={item => item.id}
+    />
+  );
+}
+
 storiesOf('SectionHeading', module)
   .addDecorator(story => <View style={style.container}>{story()}</View>)
-  .add('Heading', () => <SectionHeading title={'Heading'} />);
-
-// <SafeAreaView style={{flex: 1}}>
-//     <KeyboardAvoidingView style={{flex: 1}} behavior="padding">
-//       <SectionList
-//         sections={[{title: 'Heading'}]}
-//         renderSectionHeader={({section}) => (
-//           <SectionHeading title={section.title} />
-//         )}
-//       />
-//   </KeyboardAvoidingView>
-// </SafeAreaView>
+  .add('Heading1', () => <Heading />)
+  .add('Heading2', () => <SecondHeading />);

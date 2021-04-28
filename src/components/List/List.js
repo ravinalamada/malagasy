49981dbe83311fef2c3e2bc5d447c3dfdc80b8f6
prisca <prisca.ros@onja.org>
2021-04-28 14:28:32 +0300

    I updated list file

--- src/components/List/List.js
+++ src/components/List/List.js
@@ -1,32 +1,20 @@
 import React from 'react';
-import {StyleSheet, View, Text} from 'react-native';
-import ActionButton from '../ActionButton/ActionButton';
-// import ListItem from '../'
+import {StyleSheet, View} from 'react-native';
+import ListItem from '../ListItem/ListItem';
 
 const style = StyleSheet.create({
   container: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    justifyContent: 'space-between',
     backgroundColor: '#FFFFFF',
     borderColor: '#E5E5E5',
     borderWidth: 1,
     borderRadius: 3,
-    paddingLeft: 16,
-    paddingTop: 17,
-    paddingBottom: 17,
-    paddingRight: 20,
-  },
-  text: {
-    fontFamily: 'Inter',
-    fontStyle: 'normal',
-    fontWeight: 'normal',
-    fontSize: 16,
-    lineHeight: 19,
-    color: '#111827',
   },
 });
 
 export default function List({name}) {
-  return <View style={style.container}></View>;
+  return (
+    <View style={style.container}>
+      <ListItem name={name} />
+    </View>
+  );
 }

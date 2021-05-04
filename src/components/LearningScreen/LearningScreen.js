51faa83baff13157878757d05aec1ed22e30f809
prisca <prisca.ros@onja.org>
2021-05-04 07:31:11 +0300

    refactored this files

--- src/components/LearningScreen/LearningScreen.js
+++ src/components/LearningScreen/LearningScreen.js
@@ -1,16 +1,35 @@
-import React, {useContext} from 'react';
+import React, {useContext, useRef} from 'react';
 import {View} from 'react-native';
-import ListItem from '../ListItem/ListItem';
+import ListItem, {style} from '../ListItem/ListItem';
+import ActionButton from '../ActionButton/ActionButton';
 import {Context} from '../../util/GlobalContext';
 
 export default function LearningScreen({name, onRowPress}) {
-  const {isLightMode} = useContext(Context);
+  const {isLightMode, phrasesArr} = useContext(Context);
+  // var btn = 'Pick';
+  // let Button = btnRef.current;
+  const btnRef = useRef(null);
+  const correctAnswer = phrasesArr.answers.name.en;
+
+  function handleBtns() {
+    let btn = btnRef.current;
+    if (correctAnswer !== name) {
+      btn = 'Correct';
+      console.log(btn);
+    }
+
+    return btn;
+  }
+
   return (
     <View
       style={{
         backgroundColor: isLightMode ? '#FFFFFF' : '#111827',
       }}>
-      <ListItem name={name} />
+      <View style={style.container}>
+        <ListItem name={name} />
+        <ActionButton buttonLabel={'Pick'} onPress={handleBtns} />
+      </View>
     </View>
   );
 }
